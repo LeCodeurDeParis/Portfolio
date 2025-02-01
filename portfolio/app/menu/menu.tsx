1023afeb1c30a84f315ff0b25191408561dcc639
Paul Boisaubert <146094035+LeCodeurDeParis@users.noreply.github.com>
2025-02-01 21:28:07 +0100

    fit: added icons for menu

--- portfolio/app/menu/menu.tsx
+++ portfolio/app/menu/menu.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
+
 export default function Menu() {
   return (
-    <nav className="flex justify-center items-center w-3/4 absolute bottom-4 left-1/2 transform -translate-x-1/2 h-16 p-4 lg:hidden bg-[#021334] rounded-full">
+    <nav className="flex justify-center items-center w-3/4 lg:w-1/2 absolute bottom-4 left-1/2 transform -translate-x-1/2 h-16 p-4 bg-[#021334] rounded-full">
       <ul className="flex flex-1 justify-evenly text-white gap-2 mx-auto">
-        <li><a>Home</a></li>
-        <li><a>About</a></li>
-        <li><a>Projects</a></li>
-        <li><a>Contact</a></li>
+        <li><Image src="/accueil.png" alt="icon-accueil" width={32} height={32}/></li>
+        <li><Image src="/about.png" alt="icon-accueil" width={32} height={32}/></li>
+        <li><Image src="/projets.png" alt="icon-accueil" width={32} height={32}/></li>
+        <li><Image src="/lettre.png" alt="icon-accueil" width={32} height={32}/></li>
       </ul>
     </nav>
   );
